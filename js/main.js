3ea17c0366acd1d33ac34550cb422cb620b5ea0d
abhi tagline <aid@taglineinfotech.com>
2024-12-19 11:07:19 +0530

    Home page WIP

--- js/main.js
+++ js/main.js
@@ -1,8 +1,59 @@
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll >= 100) {
-    $(".header-wrapper").addClass("position-fixed w-100");
+    $(".header-wrapper").addClass("fixed-header position-fixed w-100");
   } else {
-    $(".header-wrapper").removeClass("position-fixed w-100");
+    $(".header-wrapper").removeClass("fixed-header position-fixed w-100");
   }
+});
+
+$(".company-slider").slick({
+  speed: 5000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  // cssEase: 'linear',
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  infinite: true,
+  initialSlide: 1,
+  arrows: false,
+  buttons: false,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.5,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 4,
+      }
+    },
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 7,
+      }
+    },
+  ]
 });
